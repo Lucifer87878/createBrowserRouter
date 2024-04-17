8244cd26e83206315dead14368c3325dc0fe4ebd
Lucifer <nallenrockar123@gmail.com>
2024-04-17 11:41:49 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,10 @@
+npm create vite@latest
+npm install -D sass
+npm install react-router-dom 
+
+
+### What is a BrowserRouter?
+
+* It is a high level react router component, that wraps the entire application.
+
+* Basically it allows us to implement navigation in react.
---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { Route, Routes } from "react-router-dom";
+
+const Home = () => <h1>Home Page</h1>;
+const AboutUs = () => <h1>About Us Page</h1>;
+
+function App() {
+  return (
+    <Routes>
+      <Route exact path="/" element={<Home />} />
+      <Route path="/about" element={<AboutUs />} />
+    </Routes>
+  );
+}
+
+
+
+export default App
